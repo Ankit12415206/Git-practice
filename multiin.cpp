10546c1a66c3f69da41f3e54383ef08a79c5bcf9
Ankit Saini <ankitsainihry@gmail.com>
2025-12-09 09:53:33 +0530

    Add files via upload

---
+++ multiin.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class LandVehicle
+{
+  public:
+    void landInfo()
+    {
+        cout << "This is a LandVehicle" << endl;
+    }
+};
+class WaterVehicle
+{
+  public:
+    void waterInfo()
+    {
+        cout << "This is a WaterVehicle" << endl;
+    }
+};
+class AmphibiousVehicle : public LandVehicle, public WaterVehicle
+{
+  public:
+    AmphibiousVehicle()
+    {
+        cout << "This is an AmphibiousVehicle" << endl;
+    }
+};
+int main()
+{
+    AmphibiousVehicle obj;
+    obj.waterInfo();
+    obj.landInfo();
+    return 0;
+}
